a994a905c88e1f66ddff967ddbeea0b5f61374cf
Anam <itsanam777@gmail.com>
2025-07-27 01:13:58 +0600

    Initial commit

---
+++ c++ap.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout <<"love "<<"\n";
+    cout <<"me"<<"\n";
+    int age=21;
+    char grade='A';
+    float cgpa=3.02f;
+    double PI=3.1416;
+    bool isSafe=true;
+    cout<<age<<"\n";
+    cout<<grade<<"\n";
+    cout<<cgpa<<"\n";
+    cout<<PI<<"\n";
+    cout<<isSafe<<"\n";
+    
+    int value=grade;
+     cout<<value<<"\n";//it prints the ASCII value of A
+     
+    float amount=12.3f;
+    int a=amount;
+     cout<<a<<"\n";
+
+    double c=44.43;
+    int cash=c;
+    cout<<cash<<"\n";
+
+    double price;
+    cin>>price;
+    cout<<"the price is " <<price<<"\n";
+    
+    int b=5/(double)2;
+    cout<<b<<"\n";
+    
+    int aa=5;
+    double ab=2.1;
+    cout<<aa/ab<<"\n";
+
+    cout<<(2>1)<<"\n";
+    return 0;
+}
